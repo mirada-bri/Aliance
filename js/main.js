ea4e931acf8441b74a6618dddd2ca67e86c343c6
Maria Chentsova <chentsova.mary@gmail.com>
2025-12-15 13:49:12 +0300

    add: swiper slider

--- js/main.js
+++ js/main.js
@@ -36,3 +36,55 @@ mMenuToggle.addEventListener('click', (event) => {
   event.preventDefault();
   menu.classList.contains('is-open') ? closeMenu() : openMenu();
 });
+
+const swiper = new Swiper('.swiper', {
+  speed: 400,
+  autoHeight: true,
+  slidesPerView: 1.5,
+  navigation: {
+    nextEl: '.slider-button-next',
+    prevEl: '.slider-button-prev',
+  },
+  breakpoints: {
+    // when window width is >= 576px
+    576: {
+      slidesPerView: 2,
+    },
+    // when window width is >= 768px
+    768: {
+      slidesPerView: 3,
+    },
+    // when window width is >= 1024px
+    1024: {
+      slidesPerView: 4,
+    },
+    // when window width is >= 1200px
+    1200: {
+      slidesPerView: 5,
+    },
+  },
+});
+
+const swiperSteps = new Swiper('.steps-slider', {
+  speed: 400,
+  loop: true,
+  slidesPerView: 1.2,
+  navigation: {
+    nextEl: '.steps-button-next',
+    prevEl: '.steps-button-prev',
+  },
+  breakpoints: {
+    // when window width is >= 576px
+    576: {
+      slidesPerView: 2,
+    },
+    // when window width is >= 768px
+    768: {
+      slidesPerView: 3,
+    },
+    // when window width is >= 1024px
+    1024: {
+      slidesPerView: 4,
+    },
+  },
+});
